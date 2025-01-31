c1618c70502f991d2fd5359281da09de25713fe3
Daniel Corn <info@cundd.net>
2025-01-31 21:05:41 +0100

    Use the Fuzzy Select for selecting the type

--- src/wizard/transaction_type.rs
+++ src/wizard/transaction_type.rs
@@ -1,7 +1,7 @@
 use crate::error::Res;
 use crate::transaction::transaction_type::TransactionType;
 use dialoguer::theme::Theme;
-use dialoguer::Select;
+use dialoguer::FuzzySelect;
 
 pub fn read_transaction_type(theme: &dyn Theme, allow_unknown: bool) -> Res<TransactionType> {
     let all = if allow_unknown {
@@ -9,7 +9,7 @@ pub fn read_transaction_type(theme: &dyn Theme, allow_unknown: bool) -> Res<Tran
     } else {
         TransactionType::all_known().to_vec()
     };
-    let i = Select::with_theme(theme)
+    let i = FuzzySelect::with_theme(theme)
         .with_prompt("Type")
         .default(0)
         .items(&all[..])
@@ -27,8 +27,8 @@ pub fn read_transaction_type_or_skip(
     } else {
         TransactionType::all_known().to_vec()
     };
-    let selection = Select::with_theme(theme)
-        .with_prompt("Select type (or press ESC or q to skip this transaction)")
+    let selection = FuzzySelect::with_theme(theme)
+        .with_prompt("Select type (or press ESC to ignore this transaction)")
         .default(0)
         .items(&all[..])
         .interact_opt()?;
